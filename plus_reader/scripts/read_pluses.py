6cab45713810e634de7b4a8e9c41d05ac23441e5
Sergey Shashkov <sh57@yandex.ru>
2017-09-14 13:58:29 +0300

    Added scrips folder for command line call

---
+++ plus_reader/scripts/read_pluses.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import argparse
+import sys
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description='Reads tables of pluses or other marks on scans (pdf or images)'
+    )
+    # TODO: Сделать нормальные параметры
+    parser.add_argument('image', nargs='+')
+    parser.add_argument('pdf', nargs='+')
+    parser.add_argument('dest', nargs='+')
+    args = parser.parse_args(args)
+
+    # TODO: Сделать вызов распознавателя
+
+if __name__ == '__main__':
+    main()
